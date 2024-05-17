233d07a3996c08c22e4951464d436e45423cdf2c
XandeBarros <xandebarrosdev@gmail.com>
2024-05-17 10:40:16 -0300

    Utilização do CORS para hospedar o banco e fe no mesmo domínio

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const apiRoutes = require('./routes/api.routes');
 
-const app = express(); app.use(express.json()); // app.use(express.cors());
+const app = express(); app.use(express.json()); app.use(cors());
 
 require('./config/db');
 
-const SERVER_PORT = 3000;
+const SERVER_PORT = 4000;
 app.use('/api', apiRoutes);
 app.get('/', (req, res) => {
   res.send('Home Page App da Academia')
